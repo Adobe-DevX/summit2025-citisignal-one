2577147785b9d5df07804329bd1b7e0e905ff574
lakshya aggarwal <laggarwal@adobe.com>
2025-01-29 13:08:53 +0530

    Updated dropin dependencies

--- scripts/__dropins__/tools/types/elsie/src/icons/index.d.ts
+++ scripts/__dropins__/tools/types/elsie/src/icons/index.d.ts
@@ -35,4 +35,7 @@ export { default as Trash } from './Trash.svg';
 export { default as Eye } from './Eye.svg';
 export { default as EyeClose } from './EyeClose.svg';
 export { default as Date } from './Date.svg';
+export { default as AddressBook } from './AddressBook.svg';
+export { default as EmptyBox } from './EmptyBox.svg';
+export { default as Coupon } from './Coupon.svg';
 //# sourceMappingURL=index.d.ts.map
